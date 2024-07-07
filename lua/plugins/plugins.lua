a6089f1ca221beffa6b70b17d859f470e0fb27cc
Amir <amirrezaghiassian@icloud.com>
2024-07-07 14:04:51 -0400

    config

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,15 @@
+local cmp = require("cmp")
+
+return {
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "moonfly",
+    },
+  },
+  { "Pocco81/auto-save.nvim", enabled = true },
+  "mg979/vim-visual-multi",
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  "mg979/vim-visual-multi",
+}
